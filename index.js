a370363b780a74ee93e97971b8d7af2148d69968
Henrique Derrico <henrique.derrico@hit.com.vc>
2023-04-23 00:17:37 -0300

    Exemplo de efeito hacker em textos

---
+++ index.js
@@ -0,0 +1,22 @@
+const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+
+document.querySelector('.word').onmouseover = event => {
+
+    var iterations = 0;
+    
+    const interval = setInterval(() => {
+        event.target.innerText = event.target.dataset.value.split("")
+        .map((letter, index) => {
+
+            if(index < iterations) return event.target.dataset.value[index];
+
+            return alphabet[Math.floor(Math.random() * alphabet.length)];
+        })
+        .join("");
+
+        if(iterations >= event.target.dataset.value.length) clearInterval(interval);
+
+        iterations += 1 / 4;
+    }, 30);
+    
+}
